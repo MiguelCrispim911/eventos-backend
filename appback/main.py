352b905ea52d5f05af35f3f5851dc03007cafda5
estefajt13 <estefajara.ej.t@gmail.com>
2025-04-26 16:58:41 -0500

    Pimer Avance todo el proyecto

--- appback/main.py
+++ appback/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 # Configurar CORS para permitir peticiones desde el frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500/"],  # URL del frontend
+    allow_origins=["http://localhost:3000"],  # URL del frontend - Cambio con react 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
