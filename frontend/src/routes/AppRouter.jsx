29ddbdb1eb5399f0cf2f224d2e56fecc795a5841
estefajt13 <estefajara.ej.t@gmail.com>
2025-05-06 02:35:52 -0500

    Cuarto Avance-todo

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,19 +1,32 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
-import Home from "../pages/Home";
-import Login from "../pages/Login";
-import Contacto from "../pages/Contacto";
-import Registro from "../pages/Registro";
-
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Contacto from "../pages/Contacto/Contacto";
+import Registro from "../pages/Registro/Registro";
+import Perfil from "../pages/Perfil/Perfil";
+import ProtectedRouteCliente from "./ProtectedRouteCliente";
 
 const AppRouter = () => (
     <MainLayout>
         <Routes>
+            {/* Rutas públicas */}
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/contacto" element={<Contacto />} />
             <Route path="/registro" element={<Registro />} />
             
+            {/* Rutas protegidas que requieren autenticación de cliente */}
+            <Route 
+                path="/perfil" 
+                element={
+                    <ProtectedRouteCliente>
+                        <Perfil />
+                    </ProtectedRouteCliente>
+                } 
+            />
+            
+            {/* Puedes agregar más rutas protegidas aquí */}
         </Routes>
     </MainLayout>
 );
